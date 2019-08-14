de7da38915dff8528dd550ac2a13d748597b1308
Julia O'Neil <julesoneil2@gmail.com>
2019-08-14 10:54:59 -0400

    icon photos and translations i guess

--- translate-it.js
+++ translate-it.js
@@ -4,19 +4,19 @@
  *
  * Copyright (c)2008 Jon Raasch. All rights reserved.
  * Released under FreeBSD License, see readme.txt
- * Do not remove the above copyright notice or text or 
+ * Do not remove the above copyright notice or text or
  * the appended link to the author.
  *
- * For more information please visit: 
+ * For more information please visit:
  * http://jonraasch.com/blog/translate-it-easy-translation-for-multiple-languages
 */
 
 TranslateIt = {
 
     /**** config ****/
-    
-    // Edit usedLangs to modify languages.  Remove or add the first comment marks (//) to iclude or disclude languages.  You can also modify the order.  
-    
+
+    // Edit usedLangs to modify languages.  Remove or add the first comment marks (//) to iclude or disclude languages.  You can also modify the order.
+
     // Make sure that all array items end with a comma, except for the last one.
     usedLangs : [
         'ar',       // Arabic
@@ -54,22 +54,22 @@ TranslateIt = {
         // 'uk',    // Ukranian
         // 'vi',    // Vietnamese
     ],
-    
-    
+
+
     // bg image location - modify this if you want to host the bg image
     bgImage : 'http://farm4.static.flickr.com/3278/3074536325_7aacba09c6.jpg',
-    
+
     // id of flags wrapper
     flagsWrap : 'translateFlags',
-    
+
     // language you will be translating FROM (default is english)
     fromLang : 'en',
-    
+
     // if you want to style the flags yourself, set this to 0
     useFlagCSS : 1,
-    
+
     /**** end config ****/
-    
+
     info : {
         'ar' : {
             'bgPos' : '0 0',
@@ -208,7 +208,7 @@ TranslateIt = {
             'fullName' : 'Vietnamese'
         }
     },
-    
+
     theFlags : [],
 
     addEvent : function(obj, evType, fn) {
@@ -219,7 +219,7 @@ TranslateIt = {
             //MSIE method
             obj['e'+evType+fn] = fn;
             obj[evType+fn] = function() {
-               obj['e'+evType+fn](window.event); 
+               obj['e'+evType+fn](window.event);
             }
             obj.attachEvent("on"+evType, obj[evType+fn]);
             return true;
@@ -230,53 +230,53 @@ TranslateIt = {
 
     translate : function ( toLang ) {
         var t = (( window.getSelection && window.getSelection() ) || ( document.getSelection && document.getSelection() ) || ( document.selection  && document.selection.createRange && document.selection.createRange().text ));
-        
+
         var e = ( document.charset || document.characterSet );
-        
+
         window.location = 'http://translate.google.com/translate' + ( t == '' ? '?u=' + escape(window.location.href) : '_t?text=' + t ) + '&hl=en&langpair=' + TranslateIt.fromLang + '|' + toLang + '&tbb=1&ie=' + e;
     },
-    
+
     whichFlag : function( theFlag ) {
         for ( var i = 0; i < TranslateIt.theFlags.length; i++ ) {
             if ( theFlag == TranslateIt.theFlags[i] ) return i;
         }
     },
-    
+
     buildFlags : function() {
         TranslateIt.flagsWrapObj = document.getElementById( TranslateIt.flagsWrap );
-                
+
         for ( var i = 0; i < TranslateIt.usedLangs.length; i++ ) {
             thisLang = TranslateIt.usedLangs[i];
-        
+
             var flag = TranslateIt.buildFlagNode( thisLang );
-            
-            
+
+
             TranslateIt.addEvent( flag, 'click', function(ev) {
                 if ( typeof( ev.preventDefault ) != 'undefined' ) ev.preventDefault();
-                
+
                 var whichFlag = TranslateIt.whichFlag( ev.target || this );
-                
+
                 var newLang = TranslateIt.usedLangs[ whichFlag ];
-                       
+
                 TranslateIt.translate( newLang );
             });
-            
+
             TranslateIt.flagsWrapObj.appendChild( flag );
             TranslateIt.theFlags.push( flag );
         }
-        
+
         TranslateIt.appendAuthorInfo();
         TranslateIt.appendClear();
-        
+
     },
-    
+
     buildFlagNode : function( thisLang ) {
         flag = document.createElement('a');
         flag.href = '#';
         flag.innerHTML = thisLang;
         flag.className = 'translate-' + thisLang;
         flag.title = 'Translate into ' + TranslateIt.info[ thisLang ].fullName;
-        
+
         if ( TranslateIt.useFlagCSS ) {
             flag.style.backgroundImage = 'url(' + TranslateIt.bgImage + ')';
             flag.style.backgroundPosition = TranslateIt.info[ thisLang ].bgPos;
@@ -291,40 +291,40 @@ TranslateIt = {
             flag.style.cssFloat = 'left';
             flag.style.styleFloat = 'left';//for ie
         }
-        
+
         return flag;
     },
-    
+
     // Please keep the link to the author, getting traffic on my blog is the reason I develop these free tools.  The link is built with Javascript so it will not effect SEO.
     appendAuthorInfo : function() {
         linkLove = document.createElement('a');
         linkLove.href = 'http://jonraasch.com/blog/translate-it-easy-translation-for-multiple-languages';
         linkLove.innerHTML = '(?)';
         linkLove.title = 'What is Translate-It?'
-        
+
         linkLove.style.fontSize = '12px';
         linkLove.style.lineHeight = '12px';
         linkLove.style.textDecoration = 'none';
         linkLove.style.marginLeft = '5px';
-        
+
         TranslateIt.flagsWrapObj.appendChild( linkLove );
     },
-    
+
     appendClear : function() {
         theClear = document.createElement('br');
         theClear.style.clear = 'both';
         theClear.style.margin = '0';
         theClear.style.padding = '0';
-        
+
         TranslateIt.flagsWrapObj.appendChild( theClear );
     },
-    
+
     init : function( flagsWrap ) {
         var theLocation = window.location.href;
-        
+
         if ( !theLocation.match(/\/translate_c\?hl\=/) ) {
             if ( typeof(flagsWrap) == 'string') TranslateIt.flagsWrap = flagsWrap;
-            
+
             TranslateIt.addEvent( window, 'load', TranslateIt.buildFlags);
         }
     }
