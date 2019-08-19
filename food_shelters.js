8c69e3a4820ab0ed440bc1aec9f2bd1f6f360faa
Julia O'Neil <julesoneil2@gmail.com>
2019-08-19 09:28:00 -0400

    twitter instagram buttons

---
+++ food_shelters.js
@@ -0,0 +1,13 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([40.7128, 74.0060]),
+      zoom: 4
+    })
+  });
+  
